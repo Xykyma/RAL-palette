89cab5301863fed6ecb559b0c969b33f4e7ac9d3
Nikita <nikitapelevin@gmail.com>
2024-03-09 22:16:36 +0300

    Генерация сетки из json

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import sys
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout
 
 class Window(QWidget):
     def __init__(self):
@@ -10,18 +10,33 @@ class Window(QWidget):
         # Загружаем форму
         uic.loadUi('form.ui', self)
 
+        self.fill_classic_pallete()
+
+
+    # Заполняем вкалдку цветами Classic
+    def fill_classic_pallete(self):
+        with open("Ral_classic.json", "r", encoding="utf-8-sig") as ral_file: 
+            ral_data = ral_file.read()
+
+            ral_classic = json.loads(ral_data)
+
+            classic_grid = QGridLayout(self.Classic_tab)
+
+            # Количество столбцов
+            num_columns = 6
+
+            # Прохожу по словарю с RAL и создаю label
+            for index, ral in enumerate(ral_classic):
+                label = QLabel(parent=self.Classic_tab)
+                label.setObjectName(ral["RAL"].replace(" ", ""))
+                row = index // num_columns
+                col = index % num_columns
+                label.setText(f"{ral['RAL']}\n{ral['English']}")
+                label.setStyleSheet(f"background-color: {ral['HEX']}")
+                classic_grid.addWidget(label, row, col)
+
 
 app = QApplication(sys.argv)
 window = Window()
 window.show()
 sys.exit(app.exec())
-
-with open("Ral_classic.json", "r", encoding="utf-8-sig") as ral_file: 
-    ral_data = ral_file.read()
-
-ral_classic = json.loads(ral_data)
-
-for ral in ral_classic:
-    print(f"RAL number - {ral['RAL']}")
-    print(f"HEX number - {ral['HEX']}")
-    print(f"RAL name - {ral['English']}")
